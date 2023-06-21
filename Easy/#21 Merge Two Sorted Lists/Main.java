bf92f2844b4624a99e92a4223b3b2e71227d2ec5
Shaik Sameer <shaiksameer515595@gmail.com>
2023-06-21 14:04:00 +0530

    saving new changes on #21

--- Easy/#21 Merge Two Sorted Lists/Main.java
+++ Easy/#21 Merge Two Sorted Lists/Main.java
@@ -3,7 +3,7 @@ import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
-        ListNode list1 = new ListNode(2);
+        ListNode list1 = new ListNode(0);
         // list1.next = new ListNode(2);
         // list1.next.next = new ListNode(4);
 
@@ -11,62 +11,25 @@ public class Main {
         // list2.next = new ListNode(3);
         // list2.next.next = new ListNode(4);
 
-        mergeTwoLists(list1, list2);
-    }
-
-    private static ListNode addNodeToLast(ListNode nodeList, int val) {
-        if (nodeList == null) {
-            nodeList = new ListNode(val);
-            return nodeList;
-        }
-        nodeList.next = addNodeToLast(nodeList.next, val);
-        return nodeList;
+        printListValuse(mergeTwoLists(list1, null));
     }
 
     public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode tempList1 = list1;
         ListNode tempList2 = list2;
 
-        ListNode finalList = (list1 == null && list2 == null) ? null : new ListNode(Integer.MAX_VALUE);
-
-        while (tempList1 != null) {
-            if (tempList2 != null) {
-                if (tempList1.val <= tempList2.val) {
-                    if (finalList.val == Integer.MAX_VALUE) {
-                        finalList.val = tempList1.val;
-                    } else {
-                        finalList.next = addNodeToLast(finalList.next, tempList1.val);
-                    }
-                    tempList1 = tempList1.next;
-                } else {
-                    if (finalList.val == Integer.MAX_VALUE) {
-                        finalList.val = tempList2.val;
-                    } else {
-                        finalList.next = addNodeToLast(finalList.next, tempList2.val);
-                    }
-                    tempList2 = tempList2.next;
-                }
-            } else {
-                if (finalList.val == Integer.MAX_VALUE) {
-                    finalList.val = tempList1.val;
-                } else {
-                    finalList.next = addNodeToLast(finalList.next, tempList1.val);
-                }
-                tempList1 = tempList1.next;
-            }
+        if (tempList1 == null) {
+            return tempList2;
+        } else if (tempList2 == null) {
+            return tempList1;
         }
-
-        while (tempList2 != null) {
-            if (finalList.val == Integer.MAX_VALUE) {
-                finalList.val = tempList2.val;
-            } else {
-                finalList.next = addNodeToLast(finalList.next, tempList2.val);
-            }
-            tempList2 = tempList2.next;
+        if (tempList1.val <= tempList2.val) {
+            tempList1.next = mergeTwoLists(tempList1.next, tempList2);
+            return tempList1;
+        } else {
+            tempList2.next = mergeTwoLists(tempList1, tempList2.next);
+            return tempList2;
         }
-
-        printListValuse(finalList);
-        return finalList;
     }
 
     private static void printListValuse(ListNode list) {
