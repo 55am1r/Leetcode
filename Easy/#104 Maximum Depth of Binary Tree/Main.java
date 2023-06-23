831472d89ffd3518dda6186498d6bebfb414a07a
Shaik Sameer <shaiksameer515595@gmail.com>
2023-06-23 16:30:56 +0530

    saving #104

---
+++ Easy/#104 Maximum Depth of Binary Tree/Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        // root.right = new TreeNode(20, new TreeNode(15), new TreeNode(7));
+        root.left = new TreeNode(9);
+
+        System.out.println(maxDepth(root));
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = maxDepth(root.left);
+        int rightHeight = maxDepth(root.right);
+        if (leftHeight > rightHeight) {
+            return leftHeight + 1;
+        } else {
+            return rightHeight + 1;
+        }
+    }
+}
